117473b9223285c034c33c7f95041b7ecec655af
Peter Gakure <petergakure97@gmail.com>
2021-12-03 10:53:13 +0300

    Create new post view function

--- gram/views.py
+++ gram/views.py
@@ -21,3 +21,19 @@ def profile(request):
     # get the profile of the current logged in user
     profile = Profile.objects.filter(user_id=current_user.id).first()
     return render(request, 'profile.html', {"posts": posts, "profile": profile})
+
+# save image  with image name,image caption and upload image to cloudinary
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    if request.method == 'POST':
+        image_name = request.POST['image_name']
+        image_caption = request.POST['image_caption']
+        image_file = request.FILES['image_file']
+        # image_file = cloudinary.uploader.upload(image_file)
+        image_url = image_file['url']
+        image = Post(image_name=image_name, image_caption=image_caption, image=image_url,
+                      profile_id=request.POST['user_id'], user_id=request.POST['user_id'])
+        image.save_image()
+        return redirect('/', {'success': 'Image Uploaded Successfully'})
+    else:
+        return render(request, 'profile.html', {'danger': 'Image Upload Failed'})
