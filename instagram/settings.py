3b2ec0a3668cd6358ed3d3c6e30568686b0a260b
Peter Gakure <petergakure97@gmail.com>
2021-12-03 13:21:56 +0300

    Modify secret key

--- instagram/settings.py
+++ instagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zl@rzbw0g-k7@q5s+pio&uj87phf9z1h(jpxo63bjc$s8n59-j'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
