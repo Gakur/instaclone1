2bb0031232429de659d80268414b422ef463fcee
Peter Gakure <petergakure97@gmail.com>
2021-12-03 10:51:12 +0300

    Create home and profile views

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+# profile page
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    # get images for the current logged in user
+    posts = Post.objects.filter(user_id=current_user.id)
+    # get the profile of the current logged in user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, 'profile.html', {"posts": posts, "profile": profile})
